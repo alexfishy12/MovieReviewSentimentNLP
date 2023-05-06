b949250a384bba59f494f755afb577eddf84014b
alexfishy12 <58009009+alexfishy12@users.noreply.github.com>
2023-05-06 12:57:42 -0400

    Updated file structure

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ jQuery(function() {
 function get_prediction(e){
     e.preventDefault()
     console.log("Receiving review...")
-    var review = $("input#review").val()
-    console.log(review)
+    var fd = new FormData();
+    var review_text = $("input#review").val()
+    fd.append('review_text', review_text)
+
+    console.log(review_text)
+
+    new Promise(function(resolve) {
+        $.ajax({
+            url: 'RunFineTunedModel.py',
+            dataType: 'text',
+            type: 'POST',
+            data: fd,
+            success: function (response, status) {
+                console.log('AJAX Success.');
+                resolve(response);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                console.log('AJAX Error:' + textStatus);
+                resolve("Error " . textStatus);
+            }
+        })
+    });
 }
